3a7da16048466ffeb255cf39439f8c3cddb24336
lihaixin <lihaixin@15099.net>
2021-02-27 07:09:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#ping 127.0.0.1
-gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&name=$GOSTNAME&mtu=$mtu&tcp=true&route=$LANRANGE&gw=$GOSTCLIENTIP"
+ping 127.0.0.1
+#gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&name=$GOSTNAME&mtu=$mtu&tcp=true&route=$LANRANGE&gw=$GOSTCLIENTIP"
 #ip route add $LANRANGE dev $GOSTNAME
-iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE;iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
-speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --fifo /root/fifo.file --interval $INTERVAL
+#iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE;iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
+#speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --fifo /root/fifo.file --interval $INTERVAL
