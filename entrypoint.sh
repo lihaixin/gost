d1e1506b31c945badbbf9315b7837a972f432cbc
lihaixin <lihaixin@15099.net>
2021-02-27 00:01:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
-gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&tcp=true
+ping 127.0.0.1
+#gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&tcp=true
 #sleep 1
 #ip route add $LANRANGE dev $GOSTNAME
 #iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE;iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
