b1c1a6f2b0ead0387d76b8197cf0ebf0787180ff
lihaixin <lihaixin@15099.net>
2021-02-26 19:31:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&tcp=true &
-sleep 2
-speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --fifo /root/fifo.file --interval $INTERVAL &
-
+sleep 1
 ip route add $LANRANGE dev $GOSTNAME
 iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE;iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
+sleep 1
+speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --fifo /root/fifo.file --interval $INTERVAL &
