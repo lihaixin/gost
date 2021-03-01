6059f6b7e56f787a7a6b932a92c909e17608b247
lihaixin <lihaixin@15099.net>
2021-03-01 16:09:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ ifconfig tun0 mtu $MTU
 ip route add $LANRANGE dev $GOSTNAME
 iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE
 iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
-speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --mtu=$MTU --fifo /root/fifo.file --interval $INTERVAL
+speederv2 -s -l0.0.0.0:4096 -r 127.0.0.1:$SERVER_PORT --fec $FEC_OPTIONS -k $PASSWORD --timeout $TIMEOUT --mtu=$MTU --fifo /root/fifo.file --interval $INTERVAL $OPTION
 echo ok
