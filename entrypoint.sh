f1f776589216c051c67b04d7b60b21757e8565db
lihaixin <lihaixin@15099.net>
2021-03-01 12:35:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #ping 127.0.0.1
-gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&tcp=true&mtu=&MTU
+gost -L tun://:$SERVER_PORT?net=$GOSTSERVERIP&tcp=true
 sleep 2
+ifconfig tun0 mtu $MTU
 ip route add $LANRANGE dev $GOSTNAME
 iptables -t nat -A POSTROUTING -o $WANNAME -s $LANRANGE -j MASQUERADE
 iptables -t nat -A POSTROUTING -o $WANNAME -s $GOSTRANGE -j MASQUERADE
